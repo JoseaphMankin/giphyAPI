6253e4f79ba8c4990c12cf524ad4a62e85ab9845
Joe Mankin <joseaphmankin@gmail.com>
2018-09-04 10:24:15 -0500

    Dynamic Buttons sorted out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
 //Wait for whole page to load before Javascript
 $(document).ready(function () {
+    const topics = ["Star Wars", "Rick and Morty", "Game of Thrones", "Cats", "Dogs", "Storybots" ];
 
+    for (let i = 0; i<topics.length; i++){
+            let topicBtn = $("<button>");
+              topicBtn.addClass('btn btn-outline-secondary mr-2 gif-btn');
+              topicBtn.attr("gif-input", topics[i]);
+              topicBtn.text(topics[i]);
+              topicBtn.attr("data-gif", topics[i]);
+              $(".gif-buttons").append(topicBtn);
+
+            //   <button class='btn btn-outline-secondary mr-2 gif-btn' data-gif="cats">Cats</button>
+
+    }
 
     const searchGif = gif => {
         $.get("https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC") .then(data => {
